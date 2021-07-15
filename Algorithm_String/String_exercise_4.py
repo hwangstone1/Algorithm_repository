5c4eb3cc8d48b0294a9726fa2a9208fb624db2c0
hwangstone1 <79895021+hwangstone1@users.noreply.github.com>
2021-07-16 02:13:44 +0900

    String_exercise_4

---
+++ Algorithm_String/String_exercise_4.py
@@ -0,0 +1,12 @@
+# 특수문자을 replace 내장함수를 통해 치환한다 .
+# 특수문자를 '.' 로 치환하여 , 문자열의 길이를 잰다.
+cro_list = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+data = input()
+for i in cro_list:
+    data = data.replace(i, '.')
+print(len(data))
+
+
+
+
+
