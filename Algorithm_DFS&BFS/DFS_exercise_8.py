2bd605e5b16c2add61b3dab2c4e4722d09aec2b9
hwangstone1 <mylee553@naver.com>
2021-08-20 17:28:43 +0900

    연산자 끼워 넣기

---
+++ Algorithm_DFS&BFS/DFS_exercise_8.py
@@ -0,0 +1,35 @@
+n = int(input())
+data = list(map(int, input().split()))
+add, sub, mul, div = map(int, input().split())
+
+min_value = 1e9
+max_value = -1e9
+
+def dfs(i,now):
+
+    global add, sub, mul, div, min_value, max_value
+    if i == n:
+        min_value = min(now,min_value)
+        max_value = max(now,max_value)
+
+    else:
+        if add > 0:
+            add -= 1
+            dfs(i+1,now+data[i])
+            add += 1
+        if sub > 0:
+            sub -= 1
+            dfs(i+1, now-data[i])
+            sub += 1
+        if mul > 0:
+            mul -= 1
+            dfs(i+1, now*data[i])
+            mul += 1
+        if div > 0:
+            div -= 1
+            dfs(i+1, int(now/data[i]))
+            div += 1
+dfs(1,data[0])
+print(max_value)
+print(min_value)
+
