bf7dc934381984f026154eb1d133efa1a5896fe4
hwangstone1 <mylee553@naver.com>
2021-09-24 23:37:03 +0900

    포도주 시식하기

---
+++ Algorithm_DP/DP_exercise_23.py
@@ -0,0 +1,15 @@
+# 포도주 시식하기
+
+n = int(input())
+
+wine = [0] * 10000
+for i in range(n):
+    wine[i] = int(input())
+dp = [0]*10000
+dp[0] = wine[0]
+dp[1] = wine[0] + wine[1]
+dp[2] = max(wine[0] + wine[2], dp[1], wine[1] + wine[2])
+for i in range(3, n):
+    dp[i] = max(dp[i-1], dp[i-2] + wine[i], dp[i-3] + wine[i-1] + wine[i])
+
+print(max(dp))
