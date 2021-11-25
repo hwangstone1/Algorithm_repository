7a759839738ef0aae8e6692d60c36f1b646a8051
hwangstone1 <mylee553@naver.com>
2021-11-25 21:15:53 +0900

    시각

---
+++ Algorithm_Implementation/구현_exercise_11.py
@@ -0,0 +1,12 @@
+# 이코테 exercise 시각 (구현)
+
+n = int(input())
+count = 0
+
+
+for i in range(n+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count +=1
+print(count)
